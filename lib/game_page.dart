bdc4d7d30c5aca4bc601eaedaefc145f741227c9
kakeru-ikeda <kakerumail1451@gmail.com>
2024-02-22 01:13:56 +0900

    refactor: _winCheck関数のロジック見直し

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -20,60 +20,81 @@ class _GamePageState extends State<GamePage> {
     ['blank', 'blank', 'blank'],
     ['blank', 'blank', 'blank'],
   ];
+
   void _playerWin({required bool turn}) {
     _winner = 'player${turn ? 1 : 2}の勝利！';
     _gameStop = true;
     _saveResult();
   }
 
-  void _winCheck(int i, int j) {
-    if (imagePathList[i][j] == 'blank' && _gameStop == false) {
-      imagePathList[i][j] = _turn ? 'maru' : 'batu';
-      if (imagePathList[0][0] == imagePathList[1][1] &&
-          imagePathList[0][0] == imagePathList[2][2] &&
-          imagePathList[0][0] != 'blank') {
-        _playerWin(turn: _turn);
-        return;
-      } else if (imagePathList[0][2] == imagePathList[1][1] &&
-          imagePathList[0][2] == imagePathList[2][0] &&
-          imagePathList[0][2] != 'blank') {
-        _playerWin(turn: _turn);
-        return;
-      }
-
-      for (int k = 0; k < 3; k++) {
-        if (imagePathList[k][0] == imagePathList[k][1] &&
-            imagePathList[k][0] == imagePathList[k][2] &&
-            imagePathList[k][0] != 'blank') {
-          _playerWin(turn: _turn);
-          return;
-        } else if (imagePathList[0][k] == imagePathList[1][k] &&
-            imagePathList[0][k] == imagePathList[2][k] &&
-            imagePathList[0][k] != 'blank') {
-          _playerWin(turn: _turn);
-          return;
-        }
-      }
-      for (int l = 0; l < imagePathList.length; l++) {
-        if (imagePathList[l].contains('blank')) {
-          _turn = !_turn;
-          return;
-        }
-      }
-      _gameStop = true;
-      _winner = '引き分け！';
-      _saveResult();
-    }
+  void _draw() {
+    _winner = '引き分け';
+    _gameStop = true;
+    _saveResult();
   }
 
   void _saveResult() async {
     DateTime now = DateTime.now();
     DateFormat dateFormater = DateFormat('yyyy-MM-dd hh:mm:ss');
     String date = dateFormater.format(now.toUtc());
+
     SharedPreferences prefs = await SharedPreferences.getInstance();
     List<String> winners = prefs.getStringList('result') ?? [];
     winners.add(date + _winner);
     prefs.setStringList('result', winners);
+  }
+
+  void _winCheck(int i, int j) {
+    /// 勝利ユーザー判定用変数
+    bool? winner;
+
+    /// 既にマスが埋まっている場合またはゲームが終了している場合は処理を行わない
+    if (imagePathList[i][j] != 'blank' || _gameStop) {
+      return;
+    }
+
+    /// タップされたマスに〇か✕の画像を表示する
+    imagePathList[i][j] = _turn ? 'maru' : 'batu';
+
+    /// 斜めの判定
+    if ((imagePathList[0][0] == imagePathList[1][1] &&
+            imagePathList[0][0] == imagePathList[2][2] &&
+            imagePathList[0][0] != 'blank') ||
+        (imagePathList[0][2] == imagePathList[1][1] &&
+            imagePathList[0][2] == imagePathList[2][0] &&
+            imagePathList[0][2] != 'blank')) {
+      winner = _turn;
+    }
+
+    /// 横と縦の判定
+    for (int k = 0; k < 3; k++) {
+      if ((imagePathList[k][0] == imagePathList[k][1] &&
+              imagePathList[k][0] == imagePathList[k][2] &&
+              imagePathList[k][0] != 'blank') ||
+          (imagePathList[0][k] == imagePathList[1][k] &&
+              imagePathList[0][k] == imagePathList[2][k] &&
+              imagePathList[0][k] != 'blank')) {
+        winner = _turn;
+      }
+    }
+
+    /// 勝利判定
+    if (winner != null) {
+      /// 勝利したプレイヤーを表示
+      _playerWin(turn: winner);
+    } else {
+      /// 引き分け判定
+      for (int l = 0; l < imagePathList.length; l++) {
+        if (imagePathList[l].contains('blank')) {
+          /// まだ埋まっていないマスがある場合はプレイヤーターンを切り替えて処理を終了
+          _turn = !_turn;
+          return;
+        }
+      }
+
+      /// 全てのマスが埋まっている場合は引き分けとする
+      _draw();
+    }
   }
 
   void _reset() {
